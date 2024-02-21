c4bc9cdfaedbe2d484126ae84071cf04b964365d
Langchun Zhou <zhoulangchun@gmail.com>
2024-02-21 16:10:05 -0500

    Added user to users

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient, PutCommand } = require('@aws-sdk/lib-dynamodb');
 const express = require('express');
+const client = new DynamoDBClient({ region: 'us-east-1' });
+const documentClient = DynamoDBDocumentClient.from(client);
+const uuid = require('uuid');
 const app = express();
-const port = 3000;
+app.use(express.json());
+const PORT = 3000;
+const USERS_TABLE = 'users';
+// const employee
 
-app.get('/', (req, res) => {
-	res.send('Hello, world!');
+// app.get('/', (req, res) => {
+// 	res.send('Hello, world!');
+// });
+
+app.post('/register', async (req, res) => {
+	console.log(req.body);
+
+	try{
+		console.log(await createUser(req.body.username, req.body.password));
+		res.sendStatus(201);
+	}catch(err){
+		res.sendStatus(500);
+	}
 });
 
-app.listen(port, () => {
-	console.log(`App listening on port ${port}.`);
+async function createUser(username, password) {
+	const command = new PutCommand({
+		TableName: USERS_TABLE,
+		Item: { id: uuid.v4(), username, password }
+	});
+
+	// try{
+	const data = await documentClient.send(command);
+	return data;
+	// }catch(error){
+
+	// }
+}
+
+app.listen(PORT, () => {
+	console.log(`App listening on port ${PORT}.`);
 });
