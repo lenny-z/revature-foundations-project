3e56582206fbc2d0dbaa7e9b95837bfc2af30120
Langchun Zhou <zhoulangchun@gmail.com>
2024-02-22 16:47:22 -0500

    Added JWT

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const authRouter = require('./routes/auth.js');
-// const { getUserByUsername, createUser } = require('./daos/userDAO.js');
 const PORT = 3000;
 
 const app = express();
 app.use(express.json());
 app.use('/auth', authRouter);
-
-// getUserByUsername('John');
-
-// app.post('/register', async (req, res) => {
-// 	// console.log(req.body);
-
-// 	try {
-// 		let data = await getUserByUsername(req.body.username);
-
-// 		if (data.Items.length > 0) {
-// 			res.sendStatus(409);
-// 		} else {
-// 			// console.log(await createUser(req.body.username, req.body.password));
-// 			await createUser(req.body.username, req.body.password);
-// 			res.sendStatus(201);
-// 		}
-// 	} catch (err) {
-// 		res.sendStatus(500);
-// 	}
-// });
 
 app.listen(PORT, () => {
 	console.log(`App listening on port ${PORT}.`);
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const userDAO = require('../daos/userDAO.js');
 const router = express.Router();
+const JWT_SECRET = process.env.JWT_SECRET;
+const NUM_SALT_ROUNDS = parseInt(process.env.NUM_SALT_ROUNDS);
 
 router.post('/register', async (req, res) => {
+	// const {username, password} = req.body;
+
 	try {
-		let data = await userDAO.getUserByUsername(req.body.username);
+		const data = await userDAO.getUserByUsername(req.body.username);
 
 		if (data.Items.length > 0) {
 			res.sendStatus(409);
 		} else {
-			// console.log(await createUser(req.body.username, req.body.password));
-			await userDAO.createUser(req.body.username, req.body.password);
+			const saltedPasswordHash = await bcrypt.hash(req.body.password, NUM_SALT_ROUNDS);
+			await userDAO.createUser(req.body.username, saltedPasswordHash);
 			res.sendStatus(201);
 		}
 	} catch (err) {
@@ -18,5 +24,39 @@ router.post('/register', async (req, res) => {
 		console.error(err);
 	}
 });
+
+router.post('/login', async (req, res) => {
+	// const {username, password} = req.body;
+
+	try {
+		const data = await userDAO.getUserByUsername(req.body.username);
+
+		if (data.Items.length === 0) {
+			res.sendStatus(400);
+		} else {
+			const user = data.Items[0];
+
+			if (await bcrypt.compare(req.body.password, user.password)) {
+				const token = jwt.sign(
+					{
+						id: user.id,
+						username: user.username,
+					},
+					JWT_SECRET,
+					{
+						expiresIn: '15m'
+					}
+				);
+
+				res.json({ token });
+			} else {
+				res.sendStatus(400);
+			}
+		}
+	} catch (err) {
+		res.sendStatus(500);
+		console.error(err);
+	}
+})
 
 module.exports = router;
