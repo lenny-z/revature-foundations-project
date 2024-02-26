c83f66b68ae9551bda43749b2d32346e45c55598
Langchun Zhou <zhoulangchun@gmail.com>
2024-02-26 11:03:19 -0500

    Tested approveTicket

--- __test__/ticketDAO.test.js
+++ __test__/ticketDAO.test.js
@@ -1,12 +1,32 @@
+require('dotenv').config();
 const ticketDAO = require('../daos/ticketDAO.js');
+const PENDING_TICKET_STATUS = process.env.PENDING_TICKET_STATUS;
+const APPROVED_TICKET_STATUS = process.env.APPROVED_TICKET_STATUS;
 
 test('createTicket', async () => {
 	const amount = Math.random() * 100;
 	const description = Math.random().toString(36);
 	await ticketDAO.createTicket(amount, description);
-	const data = await ticketDAO.getTicketsByStatus('PENDING');
+	const data = await ticketDAO.getTicketsByStatus(PENDING_TICKET_STATUS);
 	const ticket = data.Items.find(
 		ticket => ticket.amount === amount && ticket.description === description
+	);
+	expect(ticket).not.toBeNull();
+});
+
+test('approveTicket', async () => {
+	let data = await ticketDAO.getTicketsByStatus(PENDING_TICKET_STATUS);
+	console.log(data.Items);
+	const tickets = data.Items;
+	// const randomTicket = tickets[0];
+	// console.log(Math.floor(Math.random() * tickets.length));
+	const randomTicket = tickets[Math.floor(Math.random() * tickets.length)];
+	// console.log(randomTicket);
+	await ticketDAO.approveTicket(randomTicket.id);
+	data = await ticketDAO.getTicketsByStatus(APPROVED_TICKET_STATUS);
+	console.log(data);
+	const ticket = data.Items.find(
+		ticket => ticket.id === randomTicket.id && ticket.status === APPROVED_TICKET_STATUS
 	);
 	expect(ticket).not.toBeNull();
 });
--- daos/ticketDAO.js
+++ daos/ticketDAO.js
@@ -1,16 +1,23 @@
 require('dotenv').config();
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
-const { DynamoDBDocumentClient, PutCommand, QueryCommand } = require('@aws-sdk/lib-dynamodb');
+const {
+	DynamoDBDocumentClient,
+	PutCommand,
+	QueryCommand,
+	UpdateCommand
+} = require('@aws-sdk/lib-dynamodb');
 const client = new DynamoDBClient({ region: 'us-east-1' });
 const documentClient = DynamoDBDocumentClient.from(client);
 const uuid = require('uuid');
 const TICKETS_TABLE = process.env.TICKETS_TABLE;
-const STATUS_INDEX = process.env.STATUS_INDEX;
+const TICKET_STATUS_INDEX = process.env.TICKET_STATUS_INDEX;
+const PENDING_TICKET_STATUS = process.env.PENDING_TICKET_STATUS;
+const APPROVED_TICKET_STATUS = process.env.APPROVED_TICKET_STATUS;
 
 async function createTicket(amount, description) {
 	const command = new PutCommand({
 		TableName: TICKETS_TABLE,
-		Item: { id: uuid.v4(), amount, description, status: 'PENDING' }
+		Item: { id: uuid.v4(), amount, description, status: PENDING_TICKET_STATUS }
 	});
 
 	const data = await documentClient.send(command);
@@ -20,10 +27,23 @@ async function createTicket(amount, description) {
 async function getTicketsByStatus(status) {
 	const command = new QueryCommand({
 		TableName: TICKETS_TABLE,
-		IndexName: STATUS_INDEX,
-		KeyConditionExpression: '#ticketStatus = :status',
+		IndexName: TICKET_STATUS_INDEX,
+		KeyConditionExpression: '#status = :status',
 		ExpressionAttributeValues: { ':status': status },
-		ExpressionAttributeNames: { '#ticketStatus': 'status' }
+		ExpressionAttributeNames: { '#status': 'status' }
+	});
+
+	const data = await documentClient.send(command);
+	return data;
+}
+
+async function approveTicket(id) {
+	const command = new UpdateCommand({
+		TableName: TICKETS_TABLE,
+		Key: { id: id },
+		UpdateExpression: 'set #status = :status',
+		ExpressionAttributeValues: { ':status': APPROVED_TICKET_STATUS },
+		ExpressionAttributeNames: { '#status': 'status' }
 	});
 
 	const data = await documentClient.send(command);
@@ -32,5 +52,6 @@ async function getTicketsByStatus(status) {
 
 module.exports = {
 	createTicket,
-	getTicketsByStatus
+	getTicketsByStatus,
+	approveTicket
 };
